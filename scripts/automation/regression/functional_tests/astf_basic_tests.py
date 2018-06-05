9877177655497b2ced8b6e6cc2f655550a604d90
Hanoh Haim <hhaim@cisco.com>
2018-06-05 16:09:32 +0300

    WIP fix regression minor
    Signed-off-by: Hanoh Haim <hhaim@cisco.com>

--- scripts/automation/regression/functional_tests/astf_basic_tests.py
+++ scripts/automation/regression/functional_tests/astf_basic_tests.py
@@ -1,9 +1,9 @@
 import os
 from stl_basic_tests import compare_caps
-from trex_astf_lib.trex_astf_client import *   # noqa: ignore=F403
-from trex_astf_lib.trex_astf_exceptions import *   # noqa: ignore=F403
-from trex_astf_lib.cap_handling import *   # noqa: ignore=F403
-from trex_astf_lib.sim import main as sim_main
+from trex.astf.trex_astf_profile import *   # noqa: ignore=F403
+from trex.astf.trex_astf_exceptions import *   # noqa: ignore=F403
+from trex.astf.cap_handling import *   # noqa: ignore=F403
+from trex.astf.sim import main as sim_main
 import functional_general_test
 from nose.plugins.attrib import attr
 
@@ -134,7 +134,7 @@ class CAstfPcap_Test(functional_general_test.CGeneralFunctional_Test):
 
     def handle_one_cap(self, cap_file_name):
         profile = """
-from trex_astf_lib.api import ASTFProfile, ASTFCapInfo, ASTFIPGenDist, ASTFIPGen
+from trex.astf.api import ASTFProfile, ASTFCapInfo, ASTFIPGenDist, ASTFIPGen
 class Prof1():
     def __init__(self):
         pass
