df9a83f2c36714b6641e5dee7405e0020bcab7fc
Hanoh Haim <hhaim@cisco.com>
2019-10-02 09:49:16 +0300

    compile on CentOs 7.6
    Signed-off-by: Hanoh Haim <hhaim@cisco.com>

--- src/dpdk/drivers/net/tap/tap_autoconf.h
+++ src/dpdk/drivers/net/tap/tap_autoconf.h
@@ -1,34 +1,36 @@
-#ifndef HAVE_TC_FLOWER
-#undef HAVE_TC_FLOWER
-#endif
+//#ifndef HAVE_TC_FLOWER
+#define HAVE_TC_FLOWER 1
+//#endif
 
-#ifndef HAVE_TC_VLAN_ID
-#undef HAVE_TC_VLAN_ID
-#endif
+//#ifndef HAVE_TC_VLAN_ID
+//#undef  HAVE_TC_VLAN_ID
+//#endif
 
-#ifndef HAVE_TC_BPF
-#undef HAVE_TC_BPF
-#endif
+//#ifndef HAVE_TC_BPF
+#define HAVE_TC_BPF 1
+//#endif
 
-#ifndef HAVE_TC_BPF_FD
-#undef HAVE_TC_BPF_FD
-#endif
+//HAVE_TC_BPF_FD
+//#ifndef HAVE_TC_BPF_FD
+//#define  HAVE_TC_BPF_FD 1
+//#endif
 
-#ifndef HAVE_TC_ACT_BPF
-#undef HAVE_TC_ACT_BPF
-#endif
+#define HAVE_TC_VLAN_ID 1
+//#ifndef HAVE_TC_ACT_BPF
+//#undef HAVE_TC_ACT_BPF
+//#endif
 
-#ifndef HAVE_TC_ACT_BPF_FD
-#undef HAVE_TC_ACT_BPF_FD
-#endif
+//#ifndef HAVE_TC_ACT_BPF_FD
+//#undef HAVE_TC_ACT_BPF_FD
+//#endif
 
-#include "linux/if_tun.h"
+//#include "linux/if_tun.h"
 
 /* for old kernel*/
-#ifndef IFF_DETACH_QUEUE
-#define IFF_DETACH_QUEUE 0x0400
-#endif
+//#ifndef IFF_DETACH_QUEUE
+//#define IFF_DETACH_QUEUE 0x0400
+//#endif
 
-#ifndef TUNSETQUEUE
-#define TUNSETQUEUE _IOW('T', 217, int)
-#endif
+//#ifndef TUNSETQUEUE
+//#define TUNSETQUEUE _IOW('T', 217, int)
+//#endif
